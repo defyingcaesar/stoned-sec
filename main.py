fcb71bee4e35d859efb3b782ca1327c08ccc2d14
Jethro de Wet <jethro@echdesign.co.uk>
2021-09-30 19:21:10 +0200

    Amended for loop to find and print second apply link

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ results = soup.find(id="ResultsContainer")
 
 job_elements = results.find_all("div", class_="card-content")
 
-for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element.text)
-    print(company_element.text)
-    print(location_element.text)
-    print()
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+    )
+
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for job_element in python_job_elements:
+    link_url = job_element.find_all("a")[1]["href"]
+    print(f"Apply here: {link_url}\n")
