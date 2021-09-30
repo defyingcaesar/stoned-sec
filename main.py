0d77df680e950cbe503d0b99e5327b781d29ec55
Jethro de Wet <jethro@echdesign.co.uk>
2021-09-30 17:38:24 +0200

    removed index.html and added for loop / data prettify for printout

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "https://echdesign.co.uk"
-
+URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+
+job_elements = results.find_all("div", class_="card-content")
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text)
+    print(company_element.text)
+    print(location_element.text)
+    print()
